5115a1b223490dc57030b66c81b3bfb521992788
McaKrishna07 <krishvillain442@gmail.com>
2024-12-20 18:34:24 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,6 @@
+public class test
+{
+public static void main(String[] args) {
+system.out.println("**************************************"):
+system.out.prontln("Hello  from github ")
+system.out.println("**************************************")
