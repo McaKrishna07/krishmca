492a0597adace9877da09231c29202ec904e6928
McaKrishna07 <krishvillain442@gmail.com>
2024-12-20 18:38:43 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@ public class test
 {
 public static void main(String[] args) {
 system.out.println("**************************************"):
-system.out.prontln("Hello  from github ")
-system.out.println("**************************************")
+system.out.prontln("Hello  from github ");
+system.out.println("**************************************");
 }
 }
